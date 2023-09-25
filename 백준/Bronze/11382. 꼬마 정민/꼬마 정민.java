cdcfc44661895c13c90a4a9b8799d9fd5372117e
rootTiket <92284769+rootTiket@users.noreply.github.com>
2023-09-25 22:12:37 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long A,B,C;
+        A= scan.nextLong();
+        B= scan.nextLong();
+        C = scan.nextLong();
+        System.out.println(A+B+C);
+
+    }
+}
