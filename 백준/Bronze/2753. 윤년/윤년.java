4b07e202240fb6118d0a30ac0d6d5759ca3d63bd
Leeul <92284769+rootTiket@users.noreply.github.com>
2023-09-26 16:46:40 +0900

    [Bronze V] Title: 윤년, Time: 212 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a;
+        a = scan.nextInt();
+        if(a%4 == 0 && a%100 != 0)
+        {
+            System.out.println(1);
+        }
+        else if (a%400 == 0){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(0);
+        }
+}}
