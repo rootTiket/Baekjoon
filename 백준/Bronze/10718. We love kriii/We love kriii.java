271ec4ec62815edd747666c5aa69cc59a98a99e2
Leeul <92284769+rootTiket@users.noreply.github.com>
2024-01-06 23:03:02 +0900

    [Bronze V] Title: We love kriii, Time: 120 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        System.out.println("강한친구 대한육군\n강한친구 대한육군");
+    }
+}
